ee92b6ca0440f27af0bb98ea26bba306ff968e25
Tom Haynes <tomhaynes94@gmail.com>
2024-01-20 17:28:32 +0000

    project set up - remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { 
+  // useMemo, 
+  // useEffect 
+} from 'react';
+import PageTitle from './components/PageTitle';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PageTitle 
+        title={ "Hi" }
+      />
+    </>
   );
-}
+};
 
 export default App;
