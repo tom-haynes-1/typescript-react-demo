f5fc2428ad7e0a032785e8add7a9924b86e123d6
Tom Haynes <tomhaynes94@gmail.com>
2024-01-21 01:15:18 +0000

    Cleaned up repeated elements and mapped through instead to output correct number of elements

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,49 +1,44 @@
 import React from "react";
 import FormWrapper from "./FormWrapper";
+import { FormUserData, FormInputs } from "../types/FormTypes";
 
-type UserData = {
-    firstName: string;
-    lastName: string;
-    age: string;
+type Props = FormUserData & {
+  updateFields: (fields: Partial<FormUserData>) => void;
 };
 
-type Props = UserData & {
-  updateFields: (fields: Partial<UserData>) => void;
-};
-
-const UserForm = ({
-    firstName,
-    lastName,
-    age,
-    updateFields,
+const UserForm = ({ 
+    firstName, 
+    lastName, 
+    age, 
+    updateFields 
 }: Props) => {
+
+  const fields: FormInputs[] = [
+    { label: "First Name", type: "text", key: "firstName" },
+    { label: "Last Name", type: "text", key: "lastName" },
+    { label: "Age", type: "number", key: "age" },
+  ];
 
   return (
     <>
       <FormWrapper title="User Details">
-        <label>First Name</label>
-        <input
-          type="text"
-          required
-          autoFocus
-          value={firstName}
-          onChange={(e) => updateFields({ firstName: e.target.value })}
-        />
-        <label>Last Name</label>
-        <input
-          type="text"
-          required
-          value={lastName}
-          onChange={(e) => updateFields({ lastName: e.target.value })}
-        />
-        <label>Age</label>
-        <input
-          type="number"
-          required
-          min={1}
-          value={age}
-          onChange={(e) => updateFields({ age: e.target.value })}
-        />
+        {
+            fields.map((config) => (
+            <React.Fragment>
+                <label>{ config.label }</label>
+                <input
+                    type={ config.type }
+                    required={ config.type !== "number" }
+                    min={ config.type === "number" ? 1 : undefined }
+                    autoFocus={ config.key === "firstName" }
+                    value={ (config.key === "firstName" && firstName) || 
+                            (config.key === "lastName" && lastName) ||
+                            (config.key === "age" && age) || "" 
+                        }
+                    onChange={(e) => updateFields({ [config.key]: e.target.value })}
+                />
+            </React.Fragment>
+        ))}
       </FormWrapper>
     </>
   );
